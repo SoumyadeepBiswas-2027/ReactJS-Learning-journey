7043fc96d82d1c295eec3f2c08fe8434b1604d67
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-09-01 02:30:46 +0530

    Login-Page-with_ContextAPI_intigration

---
+++ LoginPg_ContextAPI/src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React ,{useState,useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Login() {
+  const[username,setUserName] = useState('');
+   const[Password,setPassword] = useState('');
+
+   const {setUser} = useContext(UserContext);
+
+  const handleSubmit =(e) =>{
+    e.preventDefault();
+    setUser({username,Password})
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" 
+      value={username}
+      onChange={(e) => setUserName(e.target.value)}
+      placeholder='username'/>
+
+      {"      "}
+
+      <input type="password" 
+       value={Password}
+      onChange={(e) => setPassword(e.target.value)}
+      placeholder='password'/>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
