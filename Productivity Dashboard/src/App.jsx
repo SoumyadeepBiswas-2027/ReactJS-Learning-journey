c75f6e5bf1ef14c473e9b1cce5e4f9157e01b6ba
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-10-03 19:02:32 +0530

    Proper styling added

--- Productivity Dashboard/src/App.jsx
+++ Productivity Dashboard/src/App.jsx
@@ -1,18 +1,28 @@
 import "./App.css";
 import IntroText from "./BitsStore/IntroText";
 import { AiFillEdit } from "react-icons/ai";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HeroStyle from "./BitsStore/Aurora";
+import TodoItems from "./components/TodoItems";
 
 function App() {
   return (
     <>
-    <IntroText className="introText"/>
-
+      <HeroStyle className="aurora-container" />
+      <IntroText className="introText" />
       <div className="inputContainer">
-        <input type="text" placeholder="what's in your mind..."
-        className="input"/>
-        <AiFillEdit className="btn" />
-      </div>
+        <input
+          type="text"
+          placeholder="what's in your mind..."
+          className="input"
+        />
+        <button className="input-btn" type="button">
+          <AiFillEdit />
+        </button>
 
+      </div>
+      <TodoItems/>
+    
     </>
   );
 }
---
+++ Productivity Dashboard/src/components/TodoItems.jsx
@@ -0,0 +1,11 @@
+function TodoItems() {
+  return (
+    <div className="listContainer">
+      <ul className="list-group">
+        <li className="list-group-item"></li>
+      </ul>
+    </div>
+  );
+}
+
+export default TodoItems;
