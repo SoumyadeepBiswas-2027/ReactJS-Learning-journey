18115c36fa428729d261c9e8a6843a85492e744d
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-10-10 23:49:48 +0530

    Complete-Btn Func Added

--- Productivity Dashboard/src/components/Complete.jsx
+++ Productivity Dashboard/src/components/Complete.jsx
@@ -1,9 +1,17 @@
 import { AiOutlineCheck } from "react-icons/ai";
+import { useState } from "react";
+
 export function Complete() {
+  const[completed, setCompleted] =useState(false);
+
+  const handleOnComplete=()=>{
+    setCompleted(true);
+  }
+
   return (
     <>
-      <button className="Complete-Btn" onClick={() => {console.log("Completed");}}>
-        <AiOutlineCheck />
+    <button className="Complete-Btn" onClick={handleOnComplete} style={{backgroundColor: completed ? "green" : " "}}>
+        {completed ? "Completed!" : <AiOutlineCheck />}
       </button>
     </>
   );
