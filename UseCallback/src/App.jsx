22233a633a25aff89d9e8a8ab6c5bc32ec1144a3
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-10-16 23:57:51 +0530

    Basic Counter implimented

--- UseCallback/src/App.jsx
+++ UseCallback/src/App.jsx
@@ -1,11 +1,27 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count,setCount]=useState(0);
+
+  function handleClick(){
+    setCount(count+1);
+  }
+ 
 
   return (
-    <div>UseCallback</div>
+   <div>
+    <button onClick={handleClick}>
+      Increment
+    </button>
+
+    <div>
+      Count: {count}
+    </div>
+
+   </div>
   )
 }
 
 export default App
+
